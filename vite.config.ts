561d26098a71ba2b87d0c88b4956dedc6589b508
TheKenkei <nowikow.witaliy@gmail.com>
2024-04-10 21:47:41 +0300

    feat: install packages for maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import cesium from 'vite-plugin-cesium';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), cesium()],
   server: {
     host: 'localhost',
     port: 3000,
