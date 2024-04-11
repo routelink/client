bec1b1206e6c61f8d6ec4bb9b104e6baaa74bb83
Kenkei <100131721+TheKenkei@users.noreply.github.com>
2024-04-11 23:13:46 +0300

    chore: add package for delete unused imports (#42)
    install `eslint-plugin-unused-imports`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,8 +9,20 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 };
