c7d71c22c0270e9d14ed6615ff931365e6857160
k939939939k <k939939939k@mail.ru>
2024-04-13 23:54:59 +0300

    feat: add-employeeManagementTable-and-buttons

---
+++ src/components/employeeManagement/AddEmployeeButton.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export function AddEmployeeButton() {
+  return (
+      <Button variant="contained" sx={{fontSize: "30px", 
+      background: "#315556",
+      borderRadius: "50%",
+      height: "56px",
+      width: "56px" }}>
+      +
+      </Button>
+  );
+}
+
--- src/components/employeeManagement/EmployeeManagement.tsx
+++ src/components/employeeManagement/EmployeeManagement.tsx
@@ -1,7 +1,27 @@
+import { EmployeeManagementTable } from './employeeManagementTable'
+import { AddEmployeeButton } from './AddEmployeeButton'
+import { Box, Stack } from '@mui/material'
+
 export function EmployeeManagement() {
     return (
-        <>
-        <p>EmployeeManagement component</p>
-        </>
+        <Box>
+            <Stack direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            spacing={2}>
+                <AddEmployeeButton />
+                <div>Сотрудник</div>
+            </Stack>
+            <Stack direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            spacing={2}
+            padding={1}>
+            </Stack>
+
+
+            <EmployeeManagementTable />
+        </Box>
     )
+
 }
