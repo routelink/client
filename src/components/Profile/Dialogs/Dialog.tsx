810faa9bdbed5bf16f8b8e1e9d9ecfe508168600
Alexander Barmin <barmin.am@gmail.com>
2024-04-21 17:23:26 +0000

    feat: change the animation of dialog boxes in profile component

--- src/components/Profile/Dialogs/Dialog.tsx
+++ src/components/Profile/Dialogs/Dialog.tsx
@@ -1,19 +1,6 @@
-import * as React from 'react';
-
 import MuiDialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { TransitionProps } from '@mui/material/transitions';
-
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & {
-    children: React.ReactElement<any, any>;
-  },
-  ref: React.Ref<unknown>,
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
 
 interface DialogProps {
   open: boolean;
@@ -29,8 +16,6 @@ export function Dialog(props: DialogProps) {
     <>
       <MuiDialog
         open={open}
-        TransitionComponent={Transition}
-        keepMounted
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description">
         <DialogTitle>{title}</DialogTitle>
