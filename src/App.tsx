45217485bc87e87e6d5166e187ad28e8e5319bb2
TheKenkei <nowikow.witaliy@gmail.com>
2024-04-28 11:05:57 +0300

    feat: add `dynamic` title
    improve `App` component
    delete unused `OtherComponent`
    add new package: `react-helmet`

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { Observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
 import {
@@ -16,37 +18,49 @@ import {
   Users,
 } from '@app/components';
 import { Main } from '@app/layouts';
-
-import './components/Auth/ChangePassword';
-import OtherContent from './layouts/OtherContent';
+import { useStore } from '@app/store';
 
 function App() {
+  const { titleStore, linksStore } = useStore();
+
   const pathname = useLocation();
   useEffect(() => {
+    titleStore.title = linksStore.getTitle(pathname.pathname);
     window.scrollTo(0, 0);
   }, [pathname]);
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Main />}>
-          <Route index path="/profile" element={<Profile />} />
-          <Route index path="/organizations" element={<Organizations />} />
-          <Route index path="/favorite" element={<OtherContent content="Favorite" />} />
-          <Route index path="/maps" element={<Maps />} />
-          <Route index path="/transport" element={<TransportManagement />} />
-          <Route index path="/employees" element={<Employees />} />
-          <Route index path="/analytics" element={<Analytics />} />
-          <Route index path="/users" element={<Users />} />
-        </Route>
-        <Route path="/auth" element={<Auth />}>
-          <Route path="login" index element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-          <Route path="change-password" element={<ChangePassword />} />
-        </Route>
-      </Routes>
-    </>
+    <Observer>
+      {() => {
+        return (
+          <>
+            <Routes>
+              <Route path="/" element={<Main />}>
+                <Route index element={<Profile />} />
+                <Route path="/organizations" element={<Organizations />} />
+                <Route path="/maps" element={<Maps />} />
+                <Route path="/transport" element={<TransportManagement />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/analytics" element={<Analytics />} />
+                <Route path="/users" element={<Users />} />
+              </Route>
+              <Route path="/auth" element={<Auth />}>
+                <Route path="login" index element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="forgot-password" element={<ForgotPassword />} />
+                <Route path="change-password" element={<ChangePassword />} />
+              </Route>
+            </Routes>
+
+            <Helmet>
+              <meta charSet="utf-8" />
+              <title> {titleStore.title} - RouteLink</title>
+              <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet>
+          </>
+        );
+      }}
+    </Observer>
   );
 }
 
