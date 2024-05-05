bf031b012f0fd72e7f62ae1c4829170d6808fa4b
Alexander Barmin <barmin.am@gmail.com>
2024-05-05 21:15:37 +0000

    feat: add profile service

--- src/stores/profile/profile.ts
+++ src/stores/profile/profile.ts
@@ -2,48 +2,67 @@ import { action, makeObservable, observable } from 'mobx';
 
 import avatar from '@app/assets/iivanov.jpg';
 import { IUser } from '@app/models';
+import { ProfileParam, ProfileService } from '@app/services';
 
 export class ProfileStore {
-    loading = true;
-    data: IUser = {
-        id: 1,
-        username: '',
-        email: '',
-        avatar: '',
-    };
-    private readonly profileService = new ProfileService();
+  loading = true;
+  data: IUser = {
+    id: 1,
+    username: '',
+    email: '',
+    avatar: '',
+  };
+  private readonly profileService = new ProfileService();
 
-    constructor() {
-        makeObservable(this, {
-            loading: observable,
-            data: observable,
-            getProfile: action,
-            changeName: action,
-            changeAvatar: action,
-        });
-    }
+  constructor() {
+    makeObservable(this, {
+      loading: observable,
+      data: observable,
+      getProfile: action,
+      changeName: action,
+      changeAvatar: action,
+    });
+  }
 
-    getProfile(): any {
-        this.loading = true;
+  async getProfile(options?: ProfileParam): Promise<void> {
+    this.loading = true;
+    return this.profileService
+      .getProfile(options)
+      .then((response) => {
+        response.data;
         this.data = {
-            id: 1,
-            username: 'Alexander M.Barmin',
-            email: 'ambarmin@vniief.ru',
-            avatar: avatar,
+          id: 1,
+          username: 'Alexander M.Barmin',
+          email: 'ambarmin@vniief.ru',
+          avatar: avatar,
         };
-    }
+      })
+      .finally(() => this.setLoading(false));
+  }
 
-    setLoading(status: boolean) {
-        this.loading = status;
-    }
+  setLoading(status: boolean) {
+    this.loading = status;
+  }
 
-    changeName(name: string): any {
-        this.loading = true;
+  async changeName(name: string, options?: ProfileParam): Promise<void> {
+    this.loading = true;
+    return this.profileService
+      .getProfile(options)
+      .then((response) => {
+        response.data;
         this.data.username = name;
-    }
+      })
+      .finally(() => this.setLoading(false));
+  }
 
-    changeAvatar(avatar: string): any {
-        this.loading = true;
+  async changeAvatar(avatar: string, options?: ProfileParam): Promise<void> {
+    this.loading = true;
+    return this.profileService
+      .getProfile(options)
+      .then((response) => {
+        response.data;
         this.data.avatar = avatar;
-    }
+      })
+      .finally(() => this.setLoading(false));
+  }
 }
