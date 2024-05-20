bc2525df237796b8988b75cf4eb1d2728d3aaf34
gk\raagaraev <raagaraev@greenatom.ru>
2024-05-20 17:44:38 +0300

    feat: proxy for requests

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_APP_BASE_URL}${import.meta.env.VITE_APP_BASE_PORT}/api`,
+  baseURL: `${import.meta.env.VITE_APP_BASE_URL}:${import.meta.env.VITE_APP_BASE_PORT}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
