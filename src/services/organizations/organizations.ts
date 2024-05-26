7d02908456ae14fee2587b67822bbb19fc9f2f2e
Chaika Andrey <hahol86@narod.ru>
2024-05-26 21:55:40 +0300

    fix: remove try-catch from `orgs` `service`

--- src/services/organizations/organizations.ts
+++ src/services/organizations/organizations.ts
@@ -6,42 +6,25 @@ import api from '../api/api';
 
 export class OrganizatonsService {
   async list(): Promise<IOrganization[]> {
-    try {
-      const response: AxiosResponse<IOrganization[]> =
-        await api.get('/api/organizations');
-      return response.data.map((org): IOrganization => {
-        return {
-          ...org,
-          createdAt: org.createdAt ? new Date(org.createdAt) : undefined,
-          updatedAt: org.updatedAt ? new Date(org.updatedAt) : undefined,
-        };
-      });
-    } catch {
-      return [];
-    }
+    const response: AxiosResponse<IOrganization[]> = await api.get('/api/organizations');
+    return response.data.map((org): IOrganization => {
+      return {
+        ...org,
+        createdAt: org.createdAt ? new Date(org.createdAt) : undefined,
+        updatedAt: org.updatedAt ? new Date(org.updatedAt) : undefined,
+      };
+    });
   }
 
   async create(name: string): Promise<void> {
-    try {
-      await api.post('/api/organizations', { name: name });
-    } catch {
-      return;
-    }
+    await api.post('/api/organizations', { name: name });
   }
 
   async update(id: number, name: string): Promise<void> {
-    try {
-      await api.patch('/api/organizations/' + id, { name: name });
-    } catch {
-      return;
-    }
+    await api.patch('/api/organizations/' + id, { name: name });
   }
 
   async remove(id: number): Promise<void> {
-    try {
-      await api.delete('/api/organizations/' + id);
-    } catch {
-      return;
-    }
+    await api.delete('/api/organizations/' + id);
   }
 }
