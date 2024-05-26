365411eb7f150ac83039e4b9c96b3b0e7dc2de2a
JSFSchaikaai <160151330+JSFSchaikaai@users.noreply.github.com>
2024-05-26 22:53:01 +0300

    chore: merge branch 'main' into feat/organizations
    feat: update component organizations
    * feat: connect component `organizations` to backend (add
    create/update/delete operations)
    * fix: remove "fantom selections" in `orgs` table rows after remove
    org(s)
    * fix: remove try-catch from orgs service
    
    ---------
    
    Co-authored-by: TheKenkei <nowikow.witaliy@gmail.com>

--- src/services/organizations/organizations.ts
+++ src/services/organizations/organizations.ts
@@ -5,24 +5,33 @@ import { IOrganization } from '@app/models';
 import api from '../api/api';
 
 export class OrganizatonsService {
-  // прежняя реализация
-  // async list<T>(): Promise<T> {
-  //   return api.get<any, T>('/api/organizations');
-  // }
-
   async list(): Promise<IOrganization[]> {
-    try {
-      const response: AxiosResponse<IOrganization[]> =
-        await api.get('/api/organizations');
-      return response.data.map((org): IOrganization => {
-        return {
-          ...org,
-          createdAt: org.createdAt ? new Date(org.createdAt) : undefined,
-          updatedAt: org.updatedAt ? new Date(org.updatedAt) : undefined,
-        };
+    return await api
+      .get('/api/organizations')
+      .then((response: AxiosResponse<IOrganization[]>) => {
+        return response.data.map((org): IOrganization => {
+          return {
+            ...org,
+            createdAt: org.createdAt ? new Date(org.createdAt) : undefined,
+            updatedAt: org.updatedAt ? new Date(org.updatedAt) : undefined,
+          };
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        return [];
       });
-    } catch {
-      return [];
-    }
+  }
+
+  async create(name: string): Promise<void> {
+    await api.post('/api/organizations', { name: name });
+  }
+
+  async update(id: number, name: string): Promise<void> {
+    await api.patch('/api/organizations/' + id, { name: name });
+  }
+
+  async remove(id: number): Promise<void> {
+    await api.delete('/api/organizations/' + id);
   }
 }
